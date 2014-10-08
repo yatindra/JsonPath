840169ea0092a0070ba3e01b0ba348da72caea39
Yatindra Adike <yadike@reputation.com>
2014-10-08 17:05:11 +0530

    Workaround for having space and hyphen character in json keys.

--- src/main/java/com/nebhale/jsonpath/internal/util/ArrayUtils.java
+++ src/main/java/com/nebhale/jsonpath/internal/util/ArrayUtils.java
@@ -1,18 +1,11 @@
-/*
- * Copyright 2013 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/* Copyright 2013 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License. */
 
 package com.nebhale.jsonpath.internal.util;
 
@@ -34,7 +27,14 @@ public final class ArrayUtils {
      */
     public static final String[] EMPTY_STRING_ARRAY = new String[0];
 
-    private static final String DELIMITERS = ", ";
+    /**
+     * The empty String {@code ""}.
+     * @since 2.0
+     */
+    public static final String EMPTY = "";
+
+    private static final String DELIMITER1 = ",";
+    private static final String DELIMITER2 = ", ";
 
     private ArrayUtils() {
     }
@@ -49,49 +49,82 @@ public final class ArrayUtils {
     }
 
     public static String[] parseAsStringArray(String value) {
-        String[] array = split(value, DELIMITERS);
-        return array;
+        if (value.contains(DELIMITER2)) {
+            return split(value, DELIMITER2);
+        } else {
+            return split(value, DELIMITER1);
+        }
     }
 
-    /**
-     * <p>
-     * Splits the provided text into an array, separators specified. This is an alternative to using StringTokenizer.
-     * </p>
-     *
-     * <p>
-     * The separator is not included in the returned String array. Adjacent separators are treated as one separator. For more control over the split use the StrTokenizer class.
-     * </p>
-     *
-     * <p>
-     * A {@code null} input String returns {@code null}. A {@code null} separatorChars splits on whitespace.
-     * </p>
-     *
-     * <pre>
-     * StringUtils.split(null, *)         = null
-     * StringUtils.split("", *)           = []
-     * StringUtils.split("abc def", null) = ["abc", "def"]
-     * StringUtils.split("abc def", " ")  = ["abc", "def"]
-     * StringUtils.split("abc  def", " ") = ["abc", "def"]
-     * StringUtils.split("ab:cd:ef", ":") = ["ab", "cd", "ef"]
-     * </pre>
-     *
-     * @param str the String to parse, may be null
-     * @param separatorChars the characters used as the delimiters, {@code null} splits on whitespace
-     * @return an array of parsed Strings, {@code null} if null String input
-     */
-    public static String[] split(final String str, final String separatorChars) {
-        return splitWorker(str, separatorChars, -1, false);
+    public static String[] split(final String str, final String separator) {
+        return split(str, separator, -1, false);
     }
 
-    /**
-     * Performs the logic for the {@code split} and {@code splitPreserveAllTokens} methods that return a maximum array length.
-     *
-     * @param str the String to parse, may be {@code null}
-     * @param separatorChars the separate character
-     * @param max the maximum number of elements to include in the array. A zero or negative value implies no limit.
-     * @param preserveAllTokens if {@code true}, adjacent separators are treated as empty token separators; if {@code false}, adjacent separators are treated as one separator.
-     * @return an array of parsed Strings, {@code null} if null String input
-     */
+    public static String[] split(final String str, final String separator, final int max, final boolean preserveAllTokens) {
+        if (str == null) {
+            return null;
+        }
+
+        final int len = str.length();
+
+        if (len == 0) {
+            return ArrayUtils.EMPTY_STRING_ARRAY;
+        }
+
+        if (separator == null || EMPTY.equals(separator)) {
+            // Split on whitespace.
+            return splitWorker(str, null, max, preserveAllTokens);
+        }
+
+        final int separatorLength = separator.length();
+
+        final ArrayList<String> substrings = new ArrayList<String>();
+        int numberOfSubstrings = 0;
+        int beg = 0;
+        int end = 0;
+        while (end < len) {
+            end = str.indexOf(separator, beg);
+
+            if (end > -1) {
+                if (end > beg) {
+                    numberOfSubstrings += 1;
+
+                    if (numberOfSubstrings == max) {
+                        end = len;
+                        substrings.add(str.substring(beg));
+                    } else {
+                        // The following is OK, because String.substring( beg, end ) excludes
+                        // the character at the position 'end'.
+                        substrings.add(str.substring(beg, end));
+
+                        // Set the starting point for the next search.
+                        // The following is equivalent to beg = end + (separatorLength - 1) + 1,
+                        // which is the right calculation:
+                        beg = end + separatorLength;
+                    }
+                } else {
+                    // We found a consecutive occurrence of the separator, so skip it.
+                    if (preserveAllTokens) {
+                        numberOfSubstrings += 1;
+                        if (numberOfSubstrings == max) {
+                            end = len;
+                            substrings.add(str.substring(beg));
+                        } else {
+                            substrings.add(EMPTY);
+                        }
+                    }
+                    beg = end + separatorLength;
+                }
+            } else {
+                // String.substring( beg ) goes from 'beg' to the end of the String.
+                substrings.add(str.substring(beg));
+                end = len;
+            }
+        }
+
+        return substrings.toArray(new String[substrings.size()]);
+    }
+
     private static String[] splitWorker(final String str, final String separatorChars, final int max, final boolean preserveAllTokens) {
         // Performance tuned for 2.0 (JDK1.4)
         // Direct code is quicker than StringTokenizer.
@@ -176,4 +209,5 @@ public final class ArrayUtils {
         }
         return list.toArray(new String[list.size()]);
     }
+
 }
